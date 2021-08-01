eea776136afcdac3ea6743d2b9c32affc9efe43f
alphakys <gys1120@naver.com>
2021-08-01 22:08:44 +0900

    포인터로 버블 정렬

--- examples/ex1.c
+++ examples/ex1.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 
+void bubbleSort(int arr[])
+{
+	int temp;
+	// 9 8 7 5 4
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			printf("%d : %d ::: %d \n", (*arr), *(arr + 1), (*arr) > *(arr + 1));
+			if (*arr > *(arr + 1))
+			{
+				temp = *arr;
+				*arr = *(arr + 1);
+				*(arr + 1) = temp;
+			}
+			*arr++;
+		}
+	}
+
+}
+
 int main()
 {
-	printf("dfdf");
+	int arr[5];
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+
+	bubbleSort(arr);
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
 	return 0;
 }
