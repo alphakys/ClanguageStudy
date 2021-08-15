77fda9f7ab114b53566dbb035173a50c174eef81
alphakys <gys1120@naver.com>
2021-08-15 19:10:42 +0900

    2차원 배열 복습

--- examples/Graph.c
+++ examples/Graph.c
@@ -3,14 +3,30 @@
 
 
 
+
+
 int main() {
 	
+	int size;
+	scanf_s("%d", &size);
 	
-	
-	
+	int** arr = (int*)malloc(sizeof(int)*size);
 
+	for (int i = 0; i < size; i++) {
+		arr[i] = (int*)malloc(sizeof(int)*size);
+	}
 
-
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			arr[i][j] = i;
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			printf("%d",arr[i][j]);
+		}
+		printf("\n");
+	}
 	
 	return 0;
 }
