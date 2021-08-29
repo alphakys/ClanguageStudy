91c1055e8a610afa8c8b6dc0e2c382e80a7ab676
alphakys <gys1120@naver.com>
2021-08-29 19:40:16 +0900

    아직 감 안잡힘

--- examples/Boj1904.c
+++ examples/Boj1904.c
@@ -6,15 +6,37 @@ int main() {
 
 	scanf_s("%d", &N);
 
-	if (N == 1) {
-		printf("%d",N);
+	while (N!=-1) {
+		if (N == 1) {
+			printf("%d",N);
+		}
+		else if (N == 2) {
+				printf("%d", N);
+			}
+		else {
+			//È¦¼ö
+			if (N % 2 != 0) {
+				int q = N / 2;
+
+				int a = q * (2 + ((q - 1) * 2));
+				int b = a / 2;
+				int c = b + 1;
+
+
+				printf("%d\n", c);
+			}
+			else {
+				//Â¦¼ö
+				int qu = (N / 2) - 1;
+
+				printf("%d\n", ((qu * (2 + (qu - 1) * 2)) / 2) + 2);
+			}
+		}
+		scanf_s("%d", &N);
 	}
-	else if (N == 2) {
-		printf("%d", N);
-	}
-	else {
-		printf("%d", (N + 1) % 15746);
-	}
+
+
+	
 	
 
 	return 0;
--- examples/Boj9184.c
+++ examples/Boj9184.c
@@ -2,71 +2,28 @@
 #include<stdlib.h>
 #include<math.h>	
 
-int w(int a, int b, int c) {
-	
-	if (a <= 0 || b <= 0 || c <= 0) {
-		//printf("1번\n");
-		return 1;
-	}
-	else if (a < b && b < c && c< 20) {
-		//printf("2번\n");
-		return pow((double)2, (double)a);
-	}else if (a > 20 || b > 20 || c > 20) {
-		//printf("3번\n");
-		return 1048576;
-	}
-	else if (a == b == c) {
-		//printf("4번\n");
-		return pow((double)2, (double)a);
-	}
-	else {
-		//printf("5번\n");
-		return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-	}
-}
-
 
 int ww(int a, int b, int c) {
 
 	if (a <= 0 || b <= 0 || c <= 0) {
-		printf("1번\n");
+		
 		return 1;
 	}
 	else if (a > 20 || b > 20 || c > 20) {
-		printf("2번\n");
+		
 		return 1048576;
 	}
 	else if (a < b && b < c && c < 20) {
-		//printf("3번\n");
-		return pow((double)2, (double)a);
+		
+		return ww(a, b, c - 1) + ww(a, b - 1, c - 1) - ww(a, b - 1, c);
 	}
 	else {
-		//printf("4번\n");
-		return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+		
+		return ww(a - 1, b, c) + ww(a - 1, b - 1, c) + ww(a - 1, b, c - 1) - ww(a - 1, b - 1, c - 1);
 	}
 }
 
-typedef struct {
-	int A;
-	int B;
-	int C;
-	int result;
-}W;
-
-void setW(W *st, int a, int b, int c, int value) {
-
-	st->A = a;
-	st->B = b;
-	st->C = c;
-	st->result = value;
-}
-
-void printW(int a, int b, int c) {
-
-}
-
-
-
+#define height  5
 
 int main() {
 	int a, b, c;
@@ -74,14 +31,46 @@ int main() {
 	
 	while (!(a==-1 && b==-1 && c==-1)) {
 		
-		printf("w(%d, %d, %d) = %d\n", a, b, c, w1(a,b,c));
+		printf("ww(%d, %d, %d) = %d\n", a, b, c, ww(a,b,c));
+		
 		scanf_s("%d %d %d", &a, &b, &c);
 	}
 	*/
-	W *arr = malloc(sizeof(W)*800);
+	
+	int width = 5;
+
+	int **arr[height];
+
+	for (int i = 0; i < height; i++) {
+		arr[i] = malloc(4*width);
+	}
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			arr[i][j] = malloc(4*5);
+		}
+	}
+
+
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			for (int k = 0; k < 5; k++) {
+				arr[i][j][k] = ww(i, j, k);
+				printf("%d,%d %d -> %d\n", i, j, k, arr[i][j][k]);
+			}
+		}
+		
+	}
+
+
+
+
+
+	//W *arr = malloc(sizeof(W)*800);
 
 	//printf("%d", sizeof(W));
-
+	/*
 	for (int i = 1; i <= 5; i++) {
 		for (int j = 1; j <= 5; j++) {
 			for (int k = 1; k <= 5; k++) {
@@ -93,6 +82,6 @@ int main() {
 
 	
 	free(arr);
-
+	*/
 	return 0;
 }
