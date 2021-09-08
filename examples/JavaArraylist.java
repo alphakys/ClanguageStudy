fc20cfc15408f6cf351cecbb5cbd2fbc276dc1cd
alphakys <gys1120@naver.com>
2021-09-08 11:18:41 +0900

    arrayList

---
+++ examples/JavaArraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class JavaArraylist{
+
+
+    public static void main(String args[]){
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(0,1);
+        arr.get(index)
+        //System.out.println(arr.get(0));
+    }
+
+}
